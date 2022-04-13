0bbe12d9ae9083ee4c396f4448ad1756e61a9368
Divmarley <nexted.marley@gmail.com>
2022-04-13 19:53:08 +0000

    13 apr 2022

---
+++ variables.py
@@ -0,0 +1,40 @@
+"""Variables
+Variables are containers for storing data values.
+
+
+"""
+
+# name = "kofi"
+# age = 12
+# height = 2.98
+# isMale = True
+# isFemale = False
+
+# bins = "rubbish"
+# print(bins)
+# print(bins)
+# print(bins)
+
+
+"""
+Many Values to Multiple Variables
+Python allows you to assign values to multiple variables in one line:
+
+Output Variables
+The Python print() function is often used to output variables.
+
+"""
+# name ,age= 3253454,30
+# print(name)
+
+"""
+Exercise:
+1.Create a variable named carname and assign the value Volvo to it.
+2.Create a variable named x and assign the value 50 to it.
+3.Remove the illegal characters in the variable name:
+    2my-first_name = "John"
+4.Insert the correct syntax to assign the same value to all three variables in one code line.
+ 
+ y 🤣 z 🤣 k 🤣 "Orange"
+
+"""
